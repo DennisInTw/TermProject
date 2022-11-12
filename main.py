c48006fa4fa8bd3345b2dca39e0b97219ba0d7d5
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-11-12 18:35:13 +0800

    看到圖片上面大概的樣子,但是還需要ground truth來計算loss

--- main.py
+++ main.py
@@ -54,10 +54,12 @@ def flow_warp(im, flow):
 
     return warp_result
 
-# MEBasic對應到paper裡的G_k convnets
-class MEBasic(nn.Module):
-    def __init__(self, level):
+
+class Spynet(nn.Module):
+    def __init__(self, pyramid_level):
         super().__init__()
+        self.pyramid_level = pyramid_level
+
         self.conv1 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=7, stride=1, padding=3)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7, stride=1, padding=3)
@@ -68,82 +70,94 @@ class MEBasic(nn.Module):
         self.relu4 = nn.ReLU()
         self.conv5 = nn.Conv2d(in_channels=16, out_channels=2, kernel_size=7, stride=1, padding=3)
 
-    def forward(self, x):
-        x = self.relu1(self.conv1(x))
-        x = self.relu2(self.conv2(x))
-        x = self.relu3(self.conv3(x))
-        x = self.relu4(self.conv4(x))
-        x = self.conv5(x)
-        return x
-
-class ME_Spynet(nn.Module):
-    def __init__(self, pyramid_level=5):
-        super().__init__()
-        self.pyramid_level = pyramid_level  # level = 0 ~ pyramid_level-1
-        #建立G_k convnets, 要注意的是MEBasic(intLevel)要傳參數進去,否則會build error,但是現在還不清楚為什麼
-        self.moduleBasic = torch.nn.ModuleList([MEBasic(intLevel) for intLevel in range(pyramid_level)])
-
-    # im1 : current frame
-    # im2 : reference frame
-    def forward(self, im1, im2):
-        logging.info(f'ME_Spynet => start working...')
+    # Vk_1 => 上一個pyramid (level=k-1)得到的predicted optical flow
+    # 但是未做upsampling
+    def forward(self, im1, im2, VK_1):
+        logging.info(f'Spynet => start working...')
 
         batchsize = im1.size()[0]
-        im1_pre = im1
-        im2_pre = im2
-        #showTensorImg(im1[0])
-        #showTensorImg(im2[0])
 
-        # 建立pyramid images
-        # im1list, im2list => index=0存放level=4的pyramid image
-        #                     index=1存放level=3的pyramid image
-        # 所以底下迴圈會建立level=3 ~ level=0的pyramid image
-        im1list = [im1_pre]
-        im2list = [im2_pre]
-        for intLevel in range(self.pyramid_level - 1):
-            im1list.append(F.avg_pool2d(im1list[intLevel], kernel_size=2, stride=2))  # , count_include_pad=False))
-            im2list.append(F.avg_pool2d(im2list[intLevel], kernel_size=2, stride=2))  # , count_include_pad=False))
+        img_width = im1.size()[3]
+        img_height = im1.size()[2]
+        device_id = im1.device
+        print(f"device_id = {device_id}")
 
-        # shape_fine => 為level=0下的im2 images
-        # zero shape => 建立一個都是0的初始flow estimation
-        shape_fine = im2list[self.pyramid_level - 1].size()
-        zeroshape = [batchsize, 2, shape_fine[2] // 2, shape_fine[3] // 2]
-        device_id = im1.device.index
-        #print(f"device_id: {device_id}")
-        flowfileds = torch.zeros(zeroshape, dtype=torch.float32, device=device_id)
+        if self.pyramid_level == 0:
+            # 產生一個都是0的optical flow
+            zeroshape = [batchsize, 2, img_height, img_width]
+            flowfileds = torch.zeros(zeroshape, dtype=torch.float32, device=device_id)
 
-        for intLevel in range(self.pyramid_level):
-            flowfiledsUpsample = bilinearupsacling(flowfileds) * 2.0
-            flowfileds = flowfiledsUpsample + self.moduleBasic[intLevel](torch.cat([im1list[self.pyramid_level - 1 - intLevel], flow_warp(im2list[self.pyramid_level - 1 - intLevel], flowfiledsUpsample),flowfiledsUpsample], 1))  # residualflow
+            net_input = torch.cat([im1, im2, flowfileds], 1)
+            print(f"net_input.shape: {net_input.shape}  type(net_input): {type(net_input)}")
 
-            u = flowfileds[0].detach().cpu().numpy()
-            print(f"{type(u)} , {u.shape} , {np.max(u)}")
-            img = flow_to_image(u, display=True)
-            plt.imshow(img)
+            out = self.relu1(self.conv1(net_input))
+            out = self.relu2(self.conv2(out))
+            out = self.relu3(self.conv3(out))
+            out = self.relu4(self.conv4(out))
+            out = self.conv5(out)
+
+            numpy_optical_flow = out[0].detach().cpu().numpy()
+            print(f"{type(numpy_optical_flow)} , {numpy_optical_flow.shape} , {np.max(numpy_optical_flow)}")
+            optical_flow_img = flow_to_image(numpy_optical_flow, display=True)
+            plt.imshow(optical_flow_img)
             plt.show()
 
-        # flowfileds => 為pyramid最頂端的optical flow
+            flowfileds += out
+        # pyramid level > 0
+        else:
+            flowfileds = VK_1
+
+            flowfiledsUpsample = bilinearupsacling(flowfileds) * 2.0
+            warp_result = flow_warp(im2, flowfiledsUpsample)
+            net_input = torch.cat([im1, warp_result, flowfiledsUpsample], 1)
+            print(f"net_input.shape: {net_input.shape}  type(net_input): {type(net_input)}")
+
+            out = self.relu1(self.conv1(net_input))
+            out = self.relu2(self.conv2(out))
+            out = self.relu3(self.conv3(out))
+            out = self.relu4(self.conv4(out))
+            out = self.conv5(out)
+
+            numpy_optical_flow = out[0].detach().cpu().numpy()
+            print(f"{type(numpy_optical_flow)} , {numpy_optical_flow.shape} , {np.max(numpy_optical_flow)}")
+            optical_flow_img = flow_to_image(numpy_optical_flow, display=True)
+            plt.imshow(optical_flow_img)
+            plt.show()
+
+            flowfileds = flowfiledsUpsample + out
+
+
         return flowfileds
 
-
-
-class VideoCompressor(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # self.imageCompressor = ImageCompressor()
-        self.opticFlow = ME_Spynet(pyramid_level=PYRAMID_LEVEL)
-
-    def forward(self, input_img, ref_img):
-        logging.info(f'VideoCompressor => start working...')
-        self.opticFlow(input_img, ref_img)
-
-
-# model => VideoCompressor()
-def training(model, device, train_loader):
+# models => Spynet()
+def training(models, device, train_loader):
+    print(f"Start training...")
     for batch_idx, input in enumerate(train_loader):
-        input_img, ref_img = input[0].to(device), input[1].to(device)
-        logging.info(f'input_img.shape: {input_img.shape} \tref_img.shape: {ref_img.shape}')
-        model(input_img, ref_img)
+        im1, im2 = input[0].to(device), input[1].to(device)
+
+        # 從最小的pyramid開始train
+        predicted_flow = None
+        for k in range(PYRAMID_LEVEL):
+            print(f'train_one_level => current level = {k}')
+
+            # 建立pyramid k的optimizer以及改成training狀態
+            optimizer = torch.optim.Adam(models[k].parameters(), lr=LEARNING_RATE)
+            models[k].train()
+            optimizer.zero_grad()
+
+            # 準備當下pyramid的image1和image2, 所以對原始大小的image做down sampling
+            im1_down_sample = im1
+            im2_down_sample = im2
+            for intLevel in range(PYRAMID_LEVEL - 1 - k):
+                im1_down_sample = F.avg_pool2d(im1_down_sample, kernel_size=2, stride=2)
+                im2_down_sample = F.avg_pool2d(im2_down_sample, kernel_size=2, stride=2)
+            print(f'type(im1_down_sample): {type(im1_down_sample)}  im1_down_sample.shape: {im1_down_sample.shape} \t type(im2_down_sample): {type(im2_down_sample)}  im2_down_sample.shape: {im2_down_sample.shape}')
+            showTensorImg(im1_down_sample[0])
+            showTensorImg(im2_down_sample[0])
+
+            predicted_flow = models[k](im1_down_sample, im2_down_sample, predicted_flow)
+            models[k].eval()
+
 
 def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -155,13 +169,12 @@ def main():
     train_dataset = DataSet(path="data/vimeo_septuplet/test.txt", im_height=IMG_HEIGHT, im_width=IMG_WIDTH)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, shuffle=True, num_workers=2, batch_size=BATCH_SIZE, pin_memory=True)
 
-    model = VideoCompressor().to(device)
-#    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    # 建立所有Spynet
+    models = torch.nn.ModuleList([Spynet(intLevel).to(device) for intLevel in range(PYRAMID_LEVEL)])
 
+    for eporch in range(EPOCHS):
+        training(models, device, train_loader)
 
-    for epoch in range(EPOCHS):
-        print(f'epoch: {epoch} start...')
-        training(model, device, train_loader)
 
 
 
