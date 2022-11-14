6b8b4f96ee6c92fe8f98b3b3e1e43ef659d63060
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-11-15 01:08:00 +0800

    Update TermProjectDataAugmentation.py

--- TermProjectDataAugmentation.py
+++ TermProjectDataAugmentation.py
@@ -1,6 +1,5 @@
 import torch
 import random
-import torch.nn.functional as F
 
 # 為什麼這裡的reference image用labels??
 # image和labels都是做完transpose的tensor
@@ -14,7 +13,7 @@ def random_crop_and_pad_image_and_labels(image, labels, size):
     padding_left, padding_right = (0, max(size[1], image_shape[2]) - image_shape[2])
     padding_top, padding_bottom = (0, max(size[0], image_shape[1]) - image_shape[1])
     padding = (padding_left, padding_right, padding_top, padding_bottom)
-    combined_pad = F.pad(combined_img, padding)
+    combined_pad = torch.nn.functional.pad(combined_img, padding)
 
     freesize0 = random.randint(0, max(size[0], image_shape[1]) - size[0])
     freesize1 = random.randint(0, max(size[1], image_shape[2]) - size[1])
