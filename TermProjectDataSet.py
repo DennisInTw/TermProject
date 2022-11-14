16b73406c3dbf76f6a20c7a9316417e53c757f47
Dennis Wong <96195038+DennisInTw@users.noreply.github.com>
2022-11-14 19:22:10 +0800

    Sync code

--- TermProjectDataSet.py
+++ TermProjectDataSet.py
@@ -5,13 +5,39 @@ import imageio.v2
 import numpy as np
 from TermProjectDataAugmentation import random_crop_and_pad_image_and_labels, random_flip
 from flowlib import read_flow
+import torchvision.transforms.functional as F
+
+
+class Compose(object):
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, *args):
+        o = args
+        for t in self.transforms:
+            o = t(*o)
+        return o
+
+class Normalize(object):
+
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img1, img2):
+        frame1 = F.normalize(img1, self.mean, self.std)
+        frame2 = F.normalize(img2, self.mean, self.std)
+
+        return (frame1, frame2)
 
 # 因為需要指定哪一張是input image,哪一張是reference image,以及做preprocessing,所以自己定義一個Data Set
 class DataSet(data.Dataset):
-    def __init__(self, filelist, im_height, im_width):
+    def __init__(self, filelist, im_height, im_width, transform):
         self.image_input_list, self.image_ref_list, self.ground_truth_list = self.get_frame(framelist=filelist)  # 得到input image file name和他的reference image file name
         self.im_height = im_height
         self.im_width = im_width
+        self.transform = transform
 
         print("dataset find image: ", len(self.image_input_list))
 
@@ -63,8 +89,8 @@ class DataSet(data.Dataset):
         #ref_image = torch.transpose(ref_image, 2, 0)
         #ref_image = torch.transpose(ref_image, 1, 2)
 
-        input_image = input_image.astype(np.float32) / 255.0
-        ref_image = ref_image.astype(np.float32) / 255.0
+        #input_image = input_image.astype(np.float32) / 255.0
+        #ref_image = ref_image.astype(np.float32) / 255.0
 
         # [h, w, c] => [c, h, w]
         # 想成將RGB三個拆開處理, 每一張(w x h) image都只有R、G、B值
@@ -75,9 +101,12 @@ class DataSet(data.Dataset):
         input_image = torch.from_numpy(input_image).float()
         ref_image = torch.from_numpy(ref_image).float()
 
+        if self.transform is not None:
+            frame1, frame2 = self.transform(input_image, ref_image)
+
         # augmentation => 替current image和reference image增加變化
         #input_image, ref_image = random_crop_and_pad_image_and_labels(input_image, ref_image, [self.im_height, self.im_width])
-        #input_image, ref_image = random_flip(input_image, ref_image)
+        #input_image, ref_image, gt_optical_flow = random_flip(input_image, ref_image, gt_optical_flow)
 
         return input_image, ref_image, gt_optical_flow
 
